e3086bed89251dd804c3a656b0a6465f4c774039
keshavmittalgit <keshavmittal7974@gmail.com>
2024-07-16 13:46:17 +0530

    working with the last code

--- src/popup.js
+++ src/popup.js
@@ -1,17 +1,6 @@
-
-
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAr7yfDKXSi3bZh5xW0hAwEwu8BQbp-rec",
-  authDomain: "clipboard-extension-63acb.firebaseapp.com",
-  projectId: "clipboard-extension-63acb",
-  storageBucket: "clipboard-extension-63acb.appspot.com",
-  messagingSenderId: "236309694102",
-  appId: "1:236309694102:web:2cbe48a255c5cbab4924b7",
-  measurementId: "G-SWFPY56YVJ"
-};
+import { firebaseConfig } from "../firebaseApiKey";
 
 
 // Initialize Firebase
@@ -61,7 +50,7 @@ onAuthStateChanged(auth, (user) => {
     toggleAuthButtons(false);
   }
 });
-
+cd
 function toggleAuthButtons(loggedIn = true) {
   if (loggedIn) {
     document.getElementById('login-container').style.display = 'none';
