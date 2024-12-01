583cd7e9889b84b0a895bde7468cc12d91bb5eae
keshavmittalgit <keshavmittal7974@gmail.com>
2024-12-02 02:13:12 +0530

    added some png files to the clipboard extension

--- src/popup.js
+++ src/popup.js
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
 import { firebaseConfig } from "../firebaseApiKey";
 
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -11,13 +10,16 @@ document.getElementById('loginButton').addEventListener('click', () => {
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
 
+  showLoading();
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       console.log(userCredential.user);
       toggleAuthButtons();
+      hideLoading();
     })
     .catch((error) => {
       console.error(error);
+      hideLoading();
     });
 });
 
@@ -25,32 +27,30 @@ document.getElementById('signupButton').addEventListener('click', () => {
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
 
+  showLoading();
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       console.log(userCredential.user);
       toggleAuthButtons();
+      hideLoading();
     })
     .catch((error) => {
       console.error(error);
+      hideLoading();
     });
 });
 
 document.getElementById('logoutButton').addEventListener('click', () => {
+  showLoading();
   signOut(auth).then(() => {
     toggleAuthButtons();
+    hideLoading();
   }).catch((error) => {
     console.error(error);
+    hideLoading();
   });
 });
 
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    toggleAuthButtons();
-  } else {
-    toggleAuthButtons(false);
-  }
-});
-cd
 function toggleAuthButtons(loggedIn = true) {
   if (loggedIn) {
     document.getElementById('login-container').style.display = 'none';
@@ -60,3 +60,28 @@ function toggleAuthButtons(loggedIn = true) {
     document.getElementById('logoutButton').style.display = 'none';
   }
 }
+
+function showLoading() {
+  document.getElementById('loading').classList.remove('hidden');
+  document.getElementById('login-container').classList.add('hidden');
+  document.getElementById('logoutButton').classList.add('hidden');
+}
+
+function hideLoading() {
+  document.getElementById('loading').classList.add('hidden');
+  document.getElementById('login-container').classList.remove('hidden');
+  document.getElementById('logoutButton').classList.remove('hidden');
+}
+
+// Show loading initially
+showLoading();
+
+// Check auth state
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    toggleAuthButtons();
+  } else {
+    toggleAuthButtons(false);
+  }
+  hideLoading(); // Hide loading once auth state is determined
+});
